58be5902191169cd85c9c457a3172db2290e7847
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-26 01:42:14 +0530

    Add files via upload

---
+++ if_else/04_short_hand_if.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+    int time=20;
+    (time < 18) ? printf("Good day"): printf("Good Evening");
+return 0;
+}
+/*
+int time = 20;
+if ( time<18){
+printf("Good Day");
+}
+else
+printf("Good Evening);}
+*/
+
