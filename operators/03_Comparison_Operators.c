6976dafe1e3da4e758deeeb81abcbda5df9322a3
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-25 23:08:54 +0530

    Add files via upload

---
+++ operators/03_Comparison_Operators.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x=9;
+    int y=11;
+
+    printf("%d\n", x>y);
+    printf("%d\n", x<y);
+    printf("%d\n", x==y);
+    printf("%d\n", x!=y);
+    printf("%d\n", x>=y);
+    printf("%d\n", x<=y);
+
+return 0;
+}
