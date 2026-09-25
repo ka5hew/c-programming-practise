0818216394e1392e6045b78c130e8369e5b42690
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-25 20:23:11 +0530

    Add files via upload

---
+++ basics/arithmetic_operators.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main ()
+{
+    int x=10;
+    int y=5;
+//addition
+    printf("%d\n", x+y);
+//subtraction
+    printf("%d\n", x-y);
+//multiplication
+    printf("%d\n", x*y);
+//division
+    printf("%d\n", x/y);
+
+return 0;
+}
