2384bc2f42af5493a880d23575009894e49b4bd3
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-25 21:05:56 +0530

    Add files via upload

---
+++ basics/data_types.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+    int a=5;
+    float b=9.63;
+    double c=8.99;
+    char letter='K';
+    char greetings[]="Hello World!";
+
+    printf("%d\n", a);
+    printf("%i\n", a);
+    printf("%f\n", b);
+    printf("%lf\n", c);
+    printf("%c\n", letter);
+    printf("%s\n", greetings);
+
+return 0;
+}
