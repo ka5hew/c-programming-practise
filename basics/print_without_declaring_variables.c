daa27112d27608a9005e7bfa2ac13e84cf05e4ec
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-25 20:09:46 +0530

    Add files via upload

---
+++ basics/print_without_declaring_variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main ()
+{
+  printf("My favorite number is: %d\n", 9);
+  printf("My favorite letter is: %c\n", 'N');
+  printf("My favourite decimal is: %.2f\n", 6.7);
+  return 0;
+}
