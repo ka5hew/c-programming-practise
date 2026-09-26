79bf98a8f9fbf39ae47981c439699fa05b26e580
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-26 14:53:23 +0530

    Add files via upload

---
+++ 5. Loop/04_for_loop.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main ()
+{
+    int i;
+    for(i=0;i<5;i++){
+    printf("%d\n",i);
+}
+return 0;
+}
+//When you know exactly how many times you want to loop through a block of code, we use the for loop instead of a while loop
