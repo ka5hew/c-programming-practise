0e2dffcfac06f7b7cd63cb052346b5cf86414a67
Kashvi Bordoloi <artistyk07@gmail.com>
2026-09-26 15:36:22 +0530

    Add files via upload

---
+++ 6. For_Loop/03_nestedloop(multiplication_table).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  int i, j;
+
+  for (i = 1; i <= 3; i++) {
+    for (j = 1; j <= 3; j++) {
+      printf("%d ", i * j);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
+/*
+1 2 3
+2 4 6
+3 6 9
+*/
